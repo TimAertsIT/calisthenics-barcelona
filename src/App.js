658be439755246a15f859eaa21233fdba6d81ee9
Tim Aerts <timaerts@Tims-MacBook-Air.local>
2023-10-23 12:26:40 +0200

    making json

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
+import data from "./data.json";
+
 function App() {
   return (
-    <p>Hello world</p>
+    <div>
+      {data.map((item) => (
+        <div key={item.id}>
+          <h2>{item.name}</h2>
+          <img src={process.env.PUBLIC_URL + item.image} alt={item.name} height="50px" />
+          <ul>
+            {item.equipment.map((equip, index) => (
+              <li key={index}>{equip}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
   );
 }
 
