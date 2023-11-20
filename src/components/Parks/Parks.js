59bac6f3fd6377b4f48b685cb951cf3318cef404
Tim Aerts <timaerts@Tims-MacBook-Air.local>
2023-11-20 17:08:17 +0100

    park selections adjusted

--- src/components/Parks/Parks.js
+++ src/components/Parks/Parks.js
@@ -4,26 +4,44 @@ import data from "../../data.json";
 function Parks({ selectedPark, setSelectedPark }) {
     return (
         <div>
-            <ParksContainer>
-                {data.map((item) => (
-                    <Park key={item.id}>
-                        <ParkName>{item.name}</ParkName>
-                        <ParkImage src={process.env.PUBLIC_URL + item.image} alt={item.name} />
-                        <ul>
-                            {item.equipment.map((equip, index) => (
-                                <li key={index}>{equip}</li>
-                            ))}
-                        </ul>
-                        <button onClick={() => setSelectedPark(selectedPark === item ? null : item)}>
-                            {selectedPark === item ? 'Deselect this park' : 'Select this park'}
-                        </button>
-                    </Park>
-                ))}
-            </ParksContainer>
+            {selectedPark ? (
+                <div>
+                    <h1>{selectedPark.name}</h1>
+                    <img src={process.env.PUBLIC_URL + selectedPark.image} alt={selectedPark.name} style={{ width: "500px" }} />
+                    <ul>
+                        {selectedPark.equipment.map((equip, index) => (
+                            <li key={index}>{equip}</li>
+                        ))}
+                        <p>More info can come here</p>
+                    </ul>
+                    <button onClick={() => setSelectedPark(null)}>
+                        Close
+                    </button>
+                </div>
+            ) : (
+                <ParksContainer>
+                    {data.map((item) => (
+                        <Park key={item.id}>
+                            <ParkName>{item.name}</ParkName>
+                            <ParkImage src={process.env.PUBLIC_URL + item.image} alt={item.name} />
+                            <ul>
+                                {item.equipment.map((equip, index) => (
+                                    <li key={index}>{equip}</li>
+                                ))}
+                            </ul>
+                            <button onClick={() => setSelectedPark(item)}>
+                                Select this park
+                            </button>
+                        </Park>
+                    ))}
+                </ParksContainer>
+            )}
         </div>
     );
 }
 
 export default Parks;
+
+
 
 
